e4cfe44aa4ec03957bdac5eb62e7c85b9e4693c5
David Routen <david@routen.io>
2026-08-26 06:22:44 -0600

    feat: add setup for pdf service through ferrum

--- config/initializers/pdf_service.rb
+++ config/initializers/pdf_service.rb
@@ -1,3 +1,11 @@
 Rails.application.config.to_prepare do
   PdfService.renderer_class = "PdfService::FerumRenderer"
 end
+
+chrome_path = ENV["GOOGLE_CHROME_BIN"] || ENV["BROWSER_PATH"]
+if chrome_path.present?
+  FerrumPdf.configure do |config|
+    config.browser_path = chrome_path
+    config[:"no-sandbox"] = nil
+  end
+end
