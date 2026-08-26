2b53b8452d4b1012c11913a32879451baffcaa6c
David Routen <david@routen.io>
2026-08-26 06:37:51 -0600

    fix: set the config options for ferrum correctly

--- config/initializers/pdf_service.rb
+++ config/initializers/pdf_service.rb
@@ -6,9 +6,10 @@ chrome_path = ENV["GOOGLE_CHROME_BIN"] || ENV["BROWSER_PATH"]
 if chrome_path.present?
   FerrumPdf.configure do |config|
     config.browser_path = chrome_path
-    config.process_timeout = 30
-    config[:"no-sandbox"] = nil
-    config[:"disable-gpu"] = nil
-    config[:"disable-dev-shm-usage"] = nil
+    config.browser_options = {
+      "no-sandbox": nil,
+      "disable-gpu": nil,
+      "disable-dev-shm-usage": nil
+    }
   end
 end
