9f39b39018d671304abba19e928a8d75457ef1c8
David Routen <david@routen.io>
2026-08-26 06:32:37 -0600

    fix: set config in prod

--- config/initializers/pdf_service.rb
+++ config/initializers/pdf_service.rb
@@ -6,6 +6,9 @@ chrome_path = ENV["GOOGLE_CHROME_BIN"] || ENV["BROWSER_PATH"]
 if chrome_path.present?
   FerrumPdf.configure do |config|
     config.browser_path = chrome_path
+    config.process_timeout = 30
     config[:"no-sandbox"] = nil
+    config[:"disable-gpu"] = nil
+    config[:"disable-dev-shm-usage"] = nil
   end
 end
